bc3d7c64c3a02460bd64077a8186de18f9c43989
qirenep <54232533+qirenep@users.noreply.github.com>
2020-02-18 14:10:52 +0900

    Add files via upload

---
+++ converters.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+
+class InvalidPlatform(commands.BadArgument):
+    pass
+
+
+class Platform(commands.Converter):
+    async def convert(self, ctx, arg):
+        x = arg.lower()
+        if x not in ["pc", "psn", "xbl"]:
+            raise InvalidPlatform()
+        return x
+
+
+class Battletag(commands.Converter):
+    async def convert(self, ctx, arg):
+        if '#' in arg:
+            arg = arg.replace('#', '-')
+        return arg
